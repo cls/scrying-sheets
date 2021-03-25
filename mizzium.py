f1f346795805238c081be033c8d54a710dbaa3c0
Connor Lane Smith <cls@lubutu.com>
2021-03-25 18:18:23 +0000

    Move class Scryfall into its own module

--- mizzium.py
+++ mizzium.py
@@ -2,27 +2,9 @@ import jinja2
 import json
 import os
 import re
-import requests
 import sys
-import time
 
-class Scryfall:
-    DEFAULT_HOST = 'https://api.scryfall.com'
-
-    def __init__(self, host=DEFAULT_HOST):
-        self._host = host
-
-    def get(self, url, params=None):
-        if url.startswith('/'):
-            url = self._host + url
-
-        time.sleep(0.1)
-
-        response = requests.get(url, params=params)
-
-        print("GET {}".format(response.url), file=sys.stderr)
-
-        return response
+from scryfall import Scryfall
 
 class CardFace:
     def __init__(self, name, url, type_line, mana_cost):
