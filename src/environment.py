e616837e9e34a0bf62f08e8a7cdc4c02b2b41705
Connor Lane Smith <cls@lubutu.com>
2025-09-09 09:52:19 +0100

    Add uv, ruff, and mypy

--- src/environment.py
+++ src/environment.py
@@ -1,9 +1,8 @@
-import re
-
 import jinja2
-import markupsafe
 
 
-loader = jinja2.FileSystemLoader('templates')
+loader = jinja2.FileSystemLoader("templates")
 
-environment = jinja2.Environment(loader=loader, autoescape=jinja2.select_autoescape(), keep_trailing_newline=True)
+environment = jinja2.Environment(
+    loader=loader, autoescape=jinja2.select_autoescape(), keep_trailing_newline=True
+)
--- src/scryfall.py
+++ src/scryfall.py
@@ -4,20 +4,20 @@ import requests
 
 
 class Scryfall:
-    DEFAULT_HOST = 'https://api.scryfall.com'
+    DEFAULT_HOST = "https://api.scryfall.com"
 
     def __init__(self, host=DEFAULT_HOST):
         self._host = host
 
     def get(self, url, params=None):
-        if url.startswith('/'):
+        if url.startswith("/"):
             url = self._host + url
 
         time.sleep(0.1)
 
         response = requests.get(url, params=params)
 
-        response.raise_for_status() # raise if not 200 OK
+        response.raise_for_status()  # raise if not 200 OK
 
         return response
 
@@ -25,22 +25,22 @@ class Scryfall:
         page = self.get(url, params=params).json()
 
         while True:
-            for elem in page['data']:
+            for elem in page["data"]:
                 yield elem
 
-            if not page['has_more']:
+            if not page["has_more"]:
                 break
 
-            page = self.get(page['next_page']).json()
+            page = self.get(page["next_page"]).json()
 
     def post(self, url, json=None):
-        if url.startswith('/'):
+        if url.startswith("/"):
             url = self._host + url
 
         time.sleep(0.1)
 
         response = requests.post(url, json=json)
 
-        response.raise_for_status() # raise if not 200 OK
+        response.raise_for_status()  # raise if not 200 OK
 
         return response
